baca4feaf76cb1c8936dd34e6d42c15b6fe9629f
1991-glitch <suzanwaithera@gmail.com>
2023-02-19 13:56:03 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Numberz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = '0';
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	putchar (0);
+	}
+	putchar ('\n');
+	return (0);
+}
