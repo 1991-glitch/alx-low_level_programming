9d9cfac6b045c66ee2f402cb9a16e50e4b755240
1991-glitch <suzanwaithera@gmail.com>
2023-02-20 00:30:12 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,12 +5,16 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int c;
+	char c;
 
-	for (c = 'z'; c <= 'a'; c++)
-	putchar (0);
-	putchar ('\n');
-	return(0);
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
 }
+
