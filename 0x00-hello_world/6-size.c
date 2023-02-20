99b38e7aa9d57637346ea869b837cb79ebf4bac6
1991-glitch <suzanwaithera@gmail.com>
2023-02-20 11:58:33 -0800

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main void
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of long int: %lu byte(s)\n",(unsigned long)sizeof(c));
+	printf("size of long long int: %lu bytes(s)\n", (unsigned long)size of(d));
+	printf("size ofa float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	return (0);
+
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Hello, printf
+size of
