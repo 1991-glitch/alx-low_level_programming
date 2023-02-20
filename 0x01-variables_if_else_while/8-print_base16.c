5b0ef7d42cca0c6fb9837ceb506334252678bba6
1991-glitch <suzanwaithera@gmail.com>
2023-02-20 01:59:26 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	char y;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar (0);
+
+	for (y = 'a'; y <= 'f'; y++)
+		putchar (0);
+
+	putchar ('\n');
+
+	return (0);
+}
