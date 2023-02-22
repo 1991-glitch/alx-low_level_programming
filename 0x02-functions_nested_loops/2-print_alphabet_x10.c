c84d60aead937cb6e78de788dee9c238154a9cea
1991-glitch <suzanwaithera@gmail.com>
2023-02-22 00:37:52 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- *print_alphabet -x10-prints alphabet 10 times
+ *print_alphabet_x10-prints alphabet 10 times
  *Return:always return 0
  */
 void print_alphabet_x10(void)
 {
 	char i;
 	char h;
-	for(h = 0;h <= 9; h++)
+
+	for (h = 0; h <= 9; h++)
 	{
-		for(i = 'a';i <= 'z';i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 	_putchar(i);
 	}
