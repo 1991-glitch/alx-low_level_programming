7ba6eb8e4bad69f5d60c37bda7424c0030656741
1991-glitch <suzanwaithera@gmail.com>
2023-02-22 01:17:48 -0800

    3-islower.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /*
- * print_alphabet - function that prints alphabet in lowercase
+ * print_alphabet - a function that prints alphabet in lowercase
+ *
  * Return always 0
  */
 void print_alphabet(void)
