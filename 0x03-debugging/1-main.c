71999beb034e2f31f3463e3d8c383718e5a7f715
1991-glitch <suzanwaithera@gmail.com>
2023-02-24 13:46:15 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
-
+ *main - checks  whether an infinite loop is incoming it avoided
+ *
+ *:( sad face if the infinite loop is printed
+ *
+ *@  \o/ means a  happy face when loop is avoided
+ *
+ * Return: 0 when loop is avoided
+ *
+ */
 int main(void)
 {
-        int i;
+int i;
 
-        printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
+i = 0;
 
-        i = 0;
-
-     /* while (i < 10)
-      /*{*/
-      /*	putchar(i);
-     /* }*/
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+/**
+ * while (i < 10)
+ * /*{* /
+ * putchar(i);
+ * /* }*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-program prints the largest of three integers
+Comment out (don’t delete it!) the part of the code that is causing the output to go into an infinite loop
