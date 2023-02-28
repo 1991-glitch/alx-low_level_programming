f72d21fbc33520f551ee7e7e0e9d6dcb2028e94f
1991-glitch <suzanwaithera@gmail.com>
2023-02-28 11:49:18 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *reset_to_98 - updates the value pointers point to to 98
+ *
+ * @n: Pointer to the integer sent to the function
+ *
+ *Return - nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
