cf1d2d61492e4645642bd91415c593e1f4ba060e
1991-glitch <suzanwaithera@gmail.com>
2023-03-06 12:56:58 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,13 +11,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	*s = b;
+	int i = 0;
 
-	while (n > 0)
+	for (; n > 0; i++)
 	{
-		*s = b;
-		s++;
-		n--;
-	}
+	s[i] = b;
+	n--;
+	}	
 	return (s);
 }
+
