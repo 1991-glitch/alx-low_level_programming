273d998f6f85f061d3aae7b6f2073bf50b1bd77b
1991-glitch <suzanwaithera@gmail.com>
2023-03-06 02:18:32 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - function fills a block of  memory with a value
+ *
+ * @s: The address of the memory to be filled
+ * * @b : value to be filled
+ * @n : No of bytes to be filled starting with *s
+ *
+ * Return: char s after memory copy
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	*s = b;
+
+	while (n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+	return (s);
+}
