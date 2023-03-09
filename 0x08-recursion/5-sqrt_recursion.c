0efa3db09676c5d9998318e54603a8d405458fcc
1991-glitch <suzanwaithera@gmail.com>
2023-03-09 13:14:11 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - prints the square root of a number
+ *
+ * @n: The integer/input
+ *
+ * Return: Square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n 
