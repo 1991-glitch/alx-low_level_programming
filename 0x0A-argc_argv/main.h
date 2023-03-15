6270bd99e9d7416cb2e02207331a6832e1ad49d5
1991-glitch <suzanwaithera@gmail.com>
2023-03-15 01:49:07 -0700

    3-mul.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,7 +3,8 @@
 
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
-
+int _atoi(char *s);
+int main(int argc, char *argv[]);
 
 
 
