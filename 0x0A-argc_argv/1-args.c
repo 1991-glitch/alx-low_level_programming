3301d44beedc0cbd0ebeaa881363182ab9e227b8
1991-glitch <suzanwaithera@gmail.com>
2023-03-15 01:30:54 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
-/**
- * main function prints the number of arguments passed to it
- *
- * returns an integer
- */
-int main(int argc, char* argv[])
-{
-	int i;
+#include "main.h"
 
-	if (argc > 0 )
-	{
-		printf("%d\n", argc);
-	}
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
 	return (0);
 }
