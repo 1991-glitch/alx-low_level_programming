1c9b6d068a5500feac9c2ac809e777230f5da5d1
1991-glitch <suzanwaithera@gmail.com>
2023-03-20 07:42:42 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ *struct dog - structure dog
+ *@name: attribute pointer to char name's dog
+ *@age: attribute float age's dog
+ *@owner: attribute pointer to char owner's dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
