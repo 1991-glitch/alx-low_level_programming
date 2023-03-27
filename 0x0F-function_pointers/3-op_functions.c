7c3dd25b8ad6675fc571a42a562fb3327778f77e
1991-glitch <suzanwaithera@gmail.com>
2023-03-27 06:13:30 -0700

    4-free_grid.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-cac.h"
+#include <stdio.h>
+
+
+/**
+ * op_add - functions adds two integers
+ * @a: First int
+ * @b: Second integer
+ * Return: Sum of both
+ */
+int op_add(int a, int b)
+{
+        return (a + b);
+}
+
+/**
+ * op_sub - function subtracts 2 integers
+ * @a: First int
+ * @b: Second int
+ * Return: Difference of both
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * int op_mul - Function multiplys two numbers
+ * @a: First int
+ * @b: Second int
+ * Return: Product of both
+ */
+int op_mul(int a, int b)
+{
+        return (a x b);
+}
+/**
+ * op_div - Function divides 2 integers
+ * @a: First int
+ * @b: Second int
+ * Return: Product of both
+ */
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - Function modulus two integers
+ * @a: First int
+ * @b: Second int
+ * Return: The product
+ */
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
