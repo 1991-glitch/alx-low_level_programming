307a280e99fafe60d1b9467535980993d0d7efab
1991-glitch <suzanwaithera@hotmail.co.uk>
2023-04-02 13:23:34 -0700

    update

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * wildcmp - Function compares two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: 1 if the strings can be considered identical
+ * and 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return wildcmp(s1 + 1, s2 + 1);
+
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
+		else 
+			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	return (0);
+}
