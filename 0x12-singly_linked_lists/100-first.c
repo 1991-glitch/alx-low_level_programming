2d48e2dc0312dcaf1f9b8cf9807f8f6777d0e0d6
1991-glitch <suzanwaithera@hotmail.co.uk>
2023-04-02 11:31:49 -0700

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
-void __attribute__((constructor)) printb4Main();
+#include "lists.h"
 
 /**
- * printb4main - runs before main function is executed
+ * printb4main - runs
+ *  before main function is executed.
  */
-void __attribute__((constructor)) printb4Main()
+void printb4main(void)
 {
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
-
