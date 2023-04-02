f85663288b1db08a9b3f48f2ae64806d847a8032
1991-glitch <suzanwaithera@hotmail.co.uk>
2023-04-02 13:27:43 -0700

    updated

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,13 +14,13 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 
 	if (*s1 == *s2)
-		return wildcmp(s1 + 1, s2 + 1);
+		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == '*')
 	{
 		if (*s1 == '\0')
 			return (wildcmp(s1, s2 + 1));
-		else 
+		else if
 			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
 	return (0);
