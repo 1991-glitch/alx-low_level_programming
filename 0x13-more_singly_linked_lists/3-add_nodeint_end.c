f8ddd7d717da167dac6bc4c5bfe773a036aad7e3
1991-glitch <suzanwaithera@hotmail.co.uk>
2023-04-04 15:36:48 -0700

    updated

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,30 +2,33 @@
 
 /**
  * add_nodeint_end - function adds a new node at the end of the list
- *
+ *@head: Pointer to the head node
  * @n: Data in the new node
  *
  * Return: Address of the new element
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *newnode, *lastnode;
-	newnode = malloc(sizeof(listint_t));
-	{
-		if (newnode == NULL)
-		return NULL;
-	}
-	newnode->n = n;
-	newnode->next = NULL;
-	{
-		if (*head == NULL)
-			*head = newnode;
-	}
+	listint_t *newnode;
+	listint_t *lastnode = *head;
 
-	while (lastnode->next != NULL)
+	newnode = malloc(sizeof(listint_t));
+	if (newnode != NULL)
 	{
-		lastnode = lastnode-> next;
+		newnode->n = n;
+		newnode->next = NULL;
+	}
+	else
+		return (NULL);
+
+	if (lastnode != NULL)
+	{
+		while (lastnode->next != NULL)
+			lastnode = lastnode->next;
+
 		lastnode->next = newnode;
 	}
+	else
+		*head = newnode;
 	return (newnode);
 }
