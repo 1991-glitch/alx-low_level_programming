73949e9cbf5cd6ba1e90841369f9bfbc14e499a9
1991-glitch <suzanwaithera@hotmail.co.uk>
2023-04-04 10:03:34 -0700

    free list using head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2- function frees a list 
+ *
+ * @head: Head node
+ *
+ * Return: Void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if ( head == NULL)
+		return;
+
+	while (current != NULL)
+	{
+		current = *head;
+		*head = (*head)-> next;
+		free (current);
+	}
+	*head = NULL;
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -7,4 +7,6 @@ a function that returns the number of elements in a linked listint_t list.
 2. Add node
 a function that adds a new node at the beginning of a listint_t list
 4. Free list
-a function that frees a listint_t list..
+a function that frees a listint_t list
+5. Free
+a function that frees a listint_t list.
